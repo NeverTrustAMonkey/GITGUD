d9b08da281d41a800ab8315812a49e3db5cf563c
Patrick Sanchez <plsanch@uw.edu>
2017-01-31 10:48:12 -0800

    Added Main page buttons

--- app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
+++ app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
@@ -3,7 +3,7 @@ package edu.tacoma.uw.plsanch.gitgud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
