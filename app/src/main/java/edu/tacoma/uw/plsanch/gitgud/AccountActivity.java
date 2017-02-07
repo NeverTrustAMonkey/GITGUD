db2bee61622f49f38ddf420fe409b69c8c55ea46
Patrick Sanchez <plsanch@uw.edu>
2017-02-07 12:30:04 -0800

    Added Main page buttons

--- app/src/main/java/edu/tacoma/uw/plsanch/gitgud/AccountActivity.java
+++ app/src/main/java/edu/tacoma/uw/plsanch/gitgud/AccountActivity.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_account);
     }
 
-    public void accountMenu(View view) {
-        Intent intent = new Intent(this,AccountActivity.class);
+    public void logIn(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
 }
