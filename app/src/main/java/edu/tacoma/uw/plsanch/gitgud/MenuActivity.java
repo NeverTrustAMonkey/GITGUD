683c6a4c977919a33097e8aaa1d1eba2c1a571f1
Izulkara <izulkara@gmail.com>
2017-02-07 11:04:59 -0800

    Failing to start new activity

--- app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
+++ app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
@@ -1,7 +1,9 @@
 package edu.tacoma.uw.plsanch.gitgud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,5 +13,19 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
 
+    }
+
+    public void startCharacter(){
+        Intent intent = new Intent(this, CharacterActivity.class);
+        startActivity(intent);
+    }
+
+    public void startLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void startGuides(){
+        Intent intent = new Intent(this, GuidesActivity.class);
+        startActivity(intent);
     }
 }
