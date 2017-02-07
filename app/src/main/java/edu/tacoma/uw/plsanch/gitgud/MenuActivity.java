7dcf1b1c33384643efc4f32834980f1daca942c2
NeverTrustAMonkey <nevertrustamonkey@gmail.com>
2017-02-07 12:45:14 -0800

    Revert "Spinners for Character Activity"

--- app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
+++ app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
@@ -1,9 +1,7 @@
 package edu.tacoma.uw.plsanch.gitgud;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,21 +9,5 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-
-    }
-
-    public void startCharacter(){
-        Intent intent = new Intent(this, CharacterActivity.class);
-        startActivity(intent);
-    }
-
-    public void startLogin(){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-    public void startGuides(){
-        Intent intent = new Intent(this, GuidesActivity.class);
-        startActivity(intent);
     }
 }
