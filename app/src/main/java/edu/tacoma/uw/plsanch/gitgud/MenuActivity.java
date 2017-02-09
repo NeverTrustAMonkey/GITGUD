1462ef7b2b7ddaf5e98efaee089323376d7f480c
Izulkara <izulkara@gmail.com>
2017-02-08 16:02:54 -0800

    Character Spinners working, basic navigation implemented.

--- app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
+++ app/src/main/java/edu/tacoma/uw/plsanch/gitgud/MenuActivity.java
@@ -1,7 +1,9 @@
 package edu.tacoma.uw.plsanch.gitgud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void startCharacter (View view){
+        Intent intent = new Intent(this, CharacterActivity.class);
+        startActivity(intent);
+    }
+
+    public void startLogin (View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void startGuide (View view){
+        Intent intent = new Intent(this, CharacterActivity.class);
+        startActivity(intent);
     }
 }
