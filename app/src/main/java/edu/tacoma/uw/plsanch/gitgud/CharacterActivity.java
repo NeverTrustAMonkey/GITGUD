d3669c095efa817d6e0c4e549f5b651924c219c9
Izulkara <izulkara@gmail.com>
2017-02-16 10:08:47 -0800

    counter info displayable

--- app/src/main/java/edu/tacoma/uw/plsanch/gitgud/CharacterActivity.java
+++ app/src/main/java/edu/tacoma/uw/plsanch/gitgud/CharacterActivity.java
@@ -1,7 +1,6 @@
 package edu.tacoma.uw.plsanch.gitgud;
 
-import android.app.Activity;
-import android.support.v7.app.AlertDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,18 +8,35 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class CharacterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinner1;
     Spinner spinner2;
+    TextView stats1;
+    TextView stats2;
+    String display1;
+    String display2;
+    Integer side;
+
+    private static final String HERO_URL
+            = "http://cssgate.insttech.washington.edu/~_450bteam9/hero.php?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
+        stats1 = (TextView) findViewById(R.id.StatView1);
+        stats2 = (TextView) findViewById(R.id.StatView2);
+
 
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
@@ -35,75 +51,282 @@ public class CharacterActivity extends AppCompatActivity implements AdapterView.
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
         spinner2.setOnItemSelectedListener(this);
-        InfoFragment infoFragment = new InfoFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, infoFragment)
-                    .addToBackStack(null)
-                    .commit();
+        stats1.setText(display1);
+        stats2.setText(display2);
+    }
+
+    public void getWeb(String theHero, String theColumn, int theSide){
+
+        String theURL = "";
+        side = theSide;//Left:Right orientation
+        theURL += HERO_URL;
+        theURL += "cmd=" + theColumn;
+        theURL += "&hero=" + theHero;
+        DownloadInfoTask task = new DownloadInfoTask();
+        task.execute(new String[]{theURL});
+    }
+
+    private class DownloadInfoTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to download the hero info, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            String theDisplay = "";
+            //theDisplay = result;
+            theDisplay += result.substring(14, result.indexOf(','));
+            result = result.substring(result.indexOf(',') + 1,result.length());
+            //theDisplay += " " + result.substring(result.indexOf(','), result.lastIndexOf(','));
+            while(!result.startsWith("\"")){
+                theDisplay += "\n" + result.substring(0, result.indexOf(','));
+                result = result.substring(result.indexOf(',') + 1, result.length());
+            }
+            theDisplay = theDisplay.substring(0,theDisplay.length() - 1);
+
+
+
+            if(side == 1){
+                display1 = theDisplay;
+            } else {
+                display2 = theDisplay;
+            }
+
+            stats1.setText(display1);
+            stats2.setText(display2);//These change the text displays.
+        }
     }
 
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
-        //Left Side Spinner
-        int position1 = spinner1.getSelectedItemPosition ();
-        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
-        switch (position1){
-            case 0:imageView1.setImageResource(R.mipmap.icon_genji);break;
-            case 1:imageView1.setImageResource(R.mipmap.icon_mccree);break;
-            case 2:imageView1.setImageResource(R.mipmap.icon_pharah);break;
-            case 3:imageView1.setImageResource(R.mipmap.icon_reaper);break;
-            case 4:imageView1.setImageResource(R.mipmap.icon_soldier76);break;
-            case 5:imageView1.setImageResource(R.mipmap.icon_sombra);break;
-            case 6:imageView1.setImageResource(R.mipmap.icon_tracer);break;
-            case 7:imageView1.setImageResource(R.mipmap.icon_bastion);break;
-            case 8:imageView1.setImageResource(R.mipmap.icon_hanzo);break;
-            case 9:imageView1.setImageResource(R.mipmap.icon_junkrat);break;
-            case 10:imageView1.setImageResource(R.mipmap.icon_mei);break;
-            case 11:imageView1.setImageResource(R.mipmap.icon_torbjorn);break;
-            case 12:imageView1.setImageResource(R.mipmap.icon_widowmaker);break;
-            case 13:imageView1.setImageResource(R.mipmap.icon_dva);break;
-            case 14:imageView1.setImageResource(R.mipmap.icon_reinhardt);break;
-            case 15:imageView1.setImageResource(R.mipmap.icon_roadhog);break;
-            case 16:imageView1.setImageResource(R.mipmap.icon_winston);break;
-            case 17:imageView1.setImageResource(R.mipmap.icon_zarya);break;
-            case 18:imageView1.setImageResource(R.mipmap.icon_ana);break;
-            case 19:imageView1.setImageResource(R.mipmap.icon_lucio);break;
-            case 20:imageView1.setImageResource(R.mipmap.icon_mercy);break;
-            case 21:imageView1.setImageResource(R.mipmap.icon_symmetra);break;
-            case 22:imageView1.setImageResource(R.mipmap.icon_zenyatta);break;
-        }
+        if(parent.getId() == 2131624054) {
+            //Left Side Spinner
+            int position1 = spinner1.getSelectedItemPosition();
+            ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+            switch (position1) {
+                case 0:
+                    imageView1.setImageResource(R.mipmap.icon_genji);
+                    getWeb("Genji", "counters", 1);
+                    break;
+                case 1:
+                    imageView1.setImageResource(R.mipmap.icon_mccree);
+                    getWeb("McCree", "counters", 1);
+                    break;
+                case 2:
+                    imageView1.setImageResource(R.mipmap.icon_pharah);
+                    getWeb("Pharah", "counters", 1);
+                    break;
+                case 3:
+                    imageView1.setImageResource(R.mipmap.icon_reaper);
+                    getWeb("Reaper", "counters", 1);
+                    break;
+                case 4:
+                    imageView1.setImageResource(R.mipmap.icon_soldier76);
+                    getWeb("Soldier76", "counters", 1);
+                    break;
+                case 5:
+                    imageView1.setImageResource(R.mipmap.icon_sombra);
+                    getWeb("Sombra", "counters", 1);
+                    break;
+                case 6:
+                    imageView1.setImageResource(R.mipmap.icon_tracer);
+                    getWeb("Tracer", "counters", 1);
+                    break;
+                case 7:
+                    imageView1.setImageResource(R.mipmap.icon_bastion);
+                    getWeb("Bastion", "counters", 1);
+                    break;
+                case 8:
+                    imageView1.setImageResource(R.mipmap.icon_hanzo);
+                    getWeb("Hanzo", "counters", 1);
+                    break;
+                case 9:
+                    imageView1.setImageResource(R.mipmap.icon_junkrat);
+                    getWeb("Junkrat", "counters", 1);
+                    break;
+                case 10:
+                    imageView1.setImageResource(R.mipmap.icon_mei);
+                    getWeb("Mei", "counters", 1);
+                    break;
+                case 11:
+                    imageView1.setImageResource(R.mipmap.icon_torbjorn);
+                    getWeb("Torbjorn", "counters", 1);
+                    break;
+                case 12:
+                    imageView1.setImageResource(R.mipmap.icon_widowmaker);
+                    getWeb("Widowmaker", "counters", 1);
+                    break;
+                case 13:
+                    imageView1.setImageResource(R.mipmap.icon_dva);
+                    getWeb("D.va", "counters", 1);
+                    break;
+                case 14:
+                    imageView1.setImageResource(R.mipmap.icon_reinhardt);
+                    getWeb("Reinhardt", "counters", 1);
+                    break;
+                case 15:
+                    imageView1.setImageResource(R.mipmap.icon_roadhog);
+                    getWeb("Roadhog", "counters", 1);
+                    break;
+                case 16:
+                    imageView1.setImageResource(R.mipmap.icon_winston);
+                    getWeb("Winston", "counters", 1);
+                    break;
+                case 17:
+                    imageView1.setImageResource(R.mipmap.icon_zarya);
+                    getWeb("Zarya", "counters", 1);
+                    break;
+                case 18:
+                    imageView1.setImageResource(R.mipmap.icon_ana);
+                    getWeb("Ana", "counters", 1);
+                    break;
+                case 19:
+                    imageView1.setImageResource(R.mipmap.icon_lucio);
+                    getWeb("Lucio", "counters", 1);
+                    break;
+                case 20:
+                    imageView1.setImageResource(R.mipmap.icon_mercy);
+                    getWeb("Mercy", "counters", 1);
+                    break;
+                case 21:
+                    imageView1.setImageResource(R.mipmap.icon_symmetra);
+                    getWeb("Symmetra", "counters", 1);
+                    break;
+                case 22:
+                    imageView1.setImageResource(R.mipmap.icon_zenyatta);
+                    getWeb("Zenyatta", "counters", 1);
+                    break;
+            }
 
+        }else{
         //Right Side Spinner
         int position2 = spinner2.getSelectedItemPosition ();
         ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
-        switch (position2){
-            case 0:imageView2.setImageResource(R.mipmap.icon_genji);break;
-            case 1:imageView2.setImageResource(R.mipmap.icon_mccree);break;
-            case 2:imageView2.setImageResource(R.mipmap.icon_pharah);break;
-            case 3:imageView2.setImageResource(R.mipmap.icon_reaper);break;
-            case 4:imageView2.setImageResource(R.mipmap.icon_soldier76);break;
-            case 5:imageView2.setImageResource(R.mipmap.icon_sombra);break;
-            case 6:imageView2.setImageResource(R.mipmap.icon_tracer);break;
-            case 7:imageView2.setImageResource(R.mipmap.icon_bastion);break;
-            case 8:imageView2.setImageResource(R.mipmap.icon_hanzo);break;
-            case 9:imageView2.setImageResource(R.mipmap.icon_junkrat);break;
-            case 10:imageView2.setImageResource(R.mipmap.icon_mei);break;
-            case 11:imageView2.setImageResource(R.mipmap.icon_torbjorn);break;
-            case 12:imageView2.setImageResource(R.mipmap.icon_widowmaker);break;
-            case 13:imageView2.setImageResource(R.mipmap.icon_dva);break;
-            case 14:imageView2.setImageResource(R.mipmap.icon_reinhardt);break;
-            case 15:imageView2.setImageResource(R.mipmap.icon_roadhog);break;
-            case 16:imageView2.setImageResource(R.mipmap.icon_winston);break;
-            case 17:imageView2.setImageResource(R.mipmap.icon_zarya);break;
-            case 18:imageView2.setImageResource(R.mipmap.icon_ana);break;
-            case 19:imageView2.setImageResource(R.mipmap.icon_lucio);break;
-            case 20:imageView2.setImageResource(R.mipmap.icon_mercy);break;
-            case 21:imageView2.setImageResource(R.mipmap.icon_symmetra);break;
-            case 22:imageView2.setImageResource(R.mipmap.icon_zenyatta);break;
+        switch (position2) {
+            case 0:
+                imageView2.setImageResource(R.mipmap.icon_genji);
+                getWeb("Genji", "counters", 2);
+                break;
+            case 1:
+                imageView2.setImageResource(R.mipmap.icon_mccree);
+                getWeb("McCree", "counters", 2);
+                break;
+            case 2:
+                imageView2.setImageResource(R.mipmap.icon_pharah);
+                getWeb("Pharah", "counters", 2);
+                break;
+            case 3:
+                imageView2.setImageResource(R.mipmap.icon_reaper);
+                getWeb("Reaper", "counters", 2);
+                break;
+            case 4:
+                imageView2.setImageResource(R.mipmap.icon_soldier76);
+                getWeb("Soldier76", "counters", 2);
+                break;
+            case 5:
+                imageView2.setImageResource(R.mipmap.icon_sombra);
+                getWeb("Sombra", "counters", 2);
+                break;
+            case 6:
+                imageView2.setImageResource(R.mipmap.icon_tracer);
+                getWeb("Tracer", "counters", 2);
+                break;
+            case 7:
+                imageView2.setImageResource(R.mipmap.icon_bastion);
+                getWeb("Bastion", "counters", 2);
+                break;
+            case 8:
+                imageView2.setImageResource(R.mipmap.icon_hanzo);
+                getWeb("Hanzo", "counters", 2);
+                break;
+            case 9:
+                imageView2.setImageResource(R.mipmap.icon_junkrat);
+                getWeb("Junkrat", "counters", 2);
+                break;
+            case 10:
+                imageView2.setImageResource(R.mipmap.icon_mei);
+                getWeb("Mei", "counters", 2);
+                break;
+            case 11:
+                imageView2.setImageResource(R.mipmap.icon_torbjorn);
+                getWeb("Torbjorn", "counters", 2);
+                break;
+            case 12:
+                imageView2.setImageResource(R.mipmap.icon_widowmaker);
+                getWeb("Widowmaker", "counters", 2);
+                break;
+            case 13:
+                imageView2.setImageResource(R.mipmap.icon_dva);
+                getWeb("D.va", "counters", 2);
+                break;
+            case 14:
+                imageView2.setImageResource(R.mipmap.icon_reinhardt);
+                getWeb("Reinhardt", "counters", 2);
+                break;
+            case 15:
+                imageView2.setImageResource(R.mipmap.icon_roadhog);
+                getWeb("Roadhog", "counters", 2);
+                break;
+            case 16:
+                imageView2.setImageResource(R.mipmap.icon_winston);
+                getWeb("Winston", "counters", 2);
+                break;
+            case 17:
+                imageView2.setImageResource(R.mipmap.icon_zarya);
+                getWeb("Zarya", "counters", 2);
+                break;
+            case 18:
+                imageView2.setImageResource(R.mipmap.icon_ana);
+                getWeb("Ana", "counters", 2);
+                break;
+            case 19:
+                imageView2.setImageResource(R.mipmap.icon_lucio);
+                getWeb("Lucio", "counters", 2);
+                break;
+            case 20:
+                imageView2.setImageResource(R.mipmap.icon_mercy);
+                getWeb("Mercy", "counters", 2);
+                break;
+            case 21:
+                imageView2.setImageResource(R.mipmap.icon_symmetra);
+                getWeb("Symmetra", "counters", 2);
+                break;
+            case 22:
+                imageView2.setImageResource(R.mipmap.icon_zenyatta);
+                getWeb("Zenyatta", "counters", 2);
+                break;
         }
+        }
+
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+
     }
 }
